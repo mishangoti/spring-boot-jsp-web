44ce552f545303983718a9ad2b7d7382a7e53a0a
mishangoti <mishan2512@gmail.com>
2023-08-04 10:55:11 -0400

    Spring boot and JSP view pages web app

---
+++ src/main/java/com/springboot/web/SpringBootJspWebApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspWebApplication.class, args);
+	}
+
+}
